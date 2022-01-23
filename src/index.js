250320e91c4bdd4b882b7d623a09ae8c9b50031d
ligalaz <89035405+ligalaz@users.noreply.github.com>
2022-01-23 22:19:34 +0300

    feat: add the solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+
+  for (let elem of str) {
+    if (elem !== new Map(bracketsConfig).get(stack[stack.length - 1])) {
+      stack.push(elem)
+    } else stack.pop(elem)
+  }
+  return !stack.length
 }
